c0d576368e24141ed3c6feaa1caa2a917d3999d1
Damian Reiter <damian.reiter@gmail.com>
2018-12-20 22:03:28 -0500

    remove gatsby filesystem plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,13 +7,6 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography.js`
