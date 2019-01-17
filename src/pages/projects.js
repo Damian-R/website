0d36ebd8329730c4664aabda95a08350f72e14d0
Damian Reiter <damian.reiter@gmail.com>
2019-01-16 22:44:28 -0500

    got hexagon grid looking right

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,9 +1,27 @@
 import React from 'react'
 import Header from '../components/Header'
+import Hexagon from 'react-hexagon'
+import styled from 'styled-components'
+import hexagonStyles from '../styles/hexagon.module.css'
+
+const HexagonGrid = styled.div`
+    padding: 0 15px;
+    margin-top: 2em;
+`
 
 const ProjectsPage = () => (
     <div>
         <Header>Projects</Header>
+        <HexagonGrid>
+            <div className={hexagonStyles.row}>
+                <Hexagon className={hexagonStyles.hexagon} flatTop={true} />
+                <Hexagon className={hexagonStyles.hexagon} flatTop={true} />
+            </div>
+            <div className={hexagonStyles.row}>
+                <Hexagon className={hexagonStyles.hexagon} flatTop={true} />
+                <Hexagon className={hexagonStyles.hexagon} flatTop={true} />
+            </div>
+        </HexagonGrid>
     </div>
 )
 
